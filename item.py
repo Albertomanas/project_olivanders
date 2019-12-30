29d1c5d80215d4b30fcba3137afd52c388d7087e
Albertomanas <amanas@cifpfbmoll.eu>
2019-12-30 20:47:53 +0100

    refactor(item.py): refactorización sellIn a sell-In

--- item.py
+++ item.py
@@ -1,9 +1,9 @@
 class Item:
-    def __init__(self, name, sellIn, quality):
+    def __init__(self, name, sell_In, quality):
         self.name = name
-        self.sellIn = sellIn
+        self.sell_In = sell_In
         self.quality = quality
 # Coje de una string name, sellIn y quality
 
     def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sellIn, self.quality)
+        return "%s, %s, %s" % (self.name, self.sell_In, self.quality)
