2d02a76ecd5f9b5ec41d6ff5e0e47711f08c579e
mmasso <mmasso@cifpfbmoll.eu>
2020-01-04 22:09:39 +0100

    test: caso test quality <0

--- test_normal_item.py
+++ test_normal_item.py
@@ -11,3 +11,9 @@ def test_update_sell_in_normal_item():
     pato = Normal_item("pato", 2, 2)
     pato.update_sell_in()
     assert pato.get_sell_in() == 1
+
+
+def test_quality_not_negative_normal_item():
+    pato = Normal_item("pato", 2, 0)
+    pato.update_item()
+    assert pato.get_item_updated() == ("pato", 1, 0)
