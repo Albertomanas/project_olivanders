610f7efba4f5e6fa10b425500c3372b5c7da52b9
mmasso <mmasso@cifpfbmoll.eu>
2020-01-05 00:05:52 +0100

    test(casos test backstage): casos test completos

--- test_Backstage_pass.py
+++ test_Backstage_pass.py
@@ -1,8 +1,31 @@
 from Backstage_pass import Backstage_pass
-from Normal_item import Normal_item
 
 
-def test_update_quality_normal_item():
+def test_update_item():
     pato = Backstage_pass("pato pass", 15, 20)
     pato.update_item()
     assert pato.get_item_updated() == ("pato pass", 14, 21)
+
+
+def test_update_item_max_quality():
+    pato = Backstage_pass("pato pass", 5, 50)
+    pato.update_item()
+    assert pato.get_item_updated() == ("pato pass", 4, 50)
+
+
+def test_update_item_ten():
+    pato = Backstage_pass("pato pass", 10, 20)
+    pato.update_item()
+    assert pato.get_item_updated() == ("pato pass", 9, 22)
+
+
+def test_update_item_sell_in_one():
+    pato = Backstage_pass("pato pass", 1, 20)
+    pato.update_item()
+    assert pato.get_item_updated() == ("pato pass", 0, 25)
+
+
+def test_update_item_passed_concert():
+    pato = Backstage_pass("pato pass", 0, 50)
+    pato.update_item()
+    assert pato.get_item_updated() == ("pato pass", -1, 0)
