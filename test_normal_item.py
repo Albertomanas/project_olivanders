fd689ef526d4d4991263b7b94f8fb6e6d7bb3e7f
mmasso <mmasso@cifpfbmoll.eu>
2020-01-04 22:40:14 +0100

    test: mas casos test varios

--- test_normal_item.py
+++ test_normal_item.py
@@ -23,3 +23,31 @@ def test_sell_in_less_zero_item():
     pato = Normal_item("pato", 0, 2)
     pato.update_item()
     assert pato.get_item_updated() == ("pato", -1, 0)
+
+# Casos test random para testear codigo
+
+
+def test_random_uno():
+    pato = Normal_item("pato", -2, 30)
+    pato.update_item()
+    assert pato.get_item_updated() == ("pato", -3, 28)
+
+
+def test_random_dos():
+    pato = Normal_item("pato", 2, 40)
+    pato.update_item()
+    assert pato.get_item_updated() == ("pato", 1, 39)
+
+
+def test_random_tres():
+    pato = Normal_item("pato", 3, 26)
+    pato.update_item()
+    assert pato.get_item_updated() == ("pato", 2, 25)
+
+# Fin "random"
+
+
+def test_quality_not_more_fifty():
+    pato = Normal_item("pato", 3, 56)
+    pato.update_item()
+    assert pato.get_item_updated() == ("pato", 2, 50)
