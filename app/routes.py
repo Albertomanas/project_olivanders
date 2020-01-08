7f0dcc4d1f3e030d50a5a54f8a5a480143fddb89
mmasso <mmasso@cifpfbmoll.eu>
2020-01-08 20:11:30 +0100

    feat: enrutadores y html

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 from app import app
 from flask import render_template
+from Lógica.Gilded_rose import Gilded_rose
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    user = 'Juan'
-    return render_template('index.html', user=user)
+@app.route('/index/<numero_de_dias>')
+def index(numero_de_dias=0):
+    items = Gilded_rose((lista_de_items, int(numero_de_dies)))
+    items.main()
+    dias = numero_de_dias
+    return render_template('index.html', items=items, dias=dias)
 # Aquí meteremos lo que queremos que le salga al usuario en cada ruta
 # especifica
