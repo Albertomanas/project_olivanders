767862489bcf6016857c7c29561a211e14706eb1
mmasso <mmasso@cifpfbmoll.eu>
2020-01-08 11:09:29 +0100

    test: test corregidos

--- Lógica/test_Gilded_rose.py
+++ Lógica/test_Gilded_rose.py
@@ -20,7 +20,7 @@ def test_guilded_rose_brie_normal():
 
 def test_all_guilded_rose():
     items = Gilded_rose([
-            Conjured("+5 Dexterity Vest", 10, 20),
+            Normal_item("+5 Dexterity Vest", 10, 20),
             Aged_brie("Aged Brie", 2, 0),
             Normal_item("Elixir of the Mongoose", 5, 7),
             Sulfuras_hand("Sulfuras, Hand of Ragnaros", 0, 80),
@@ -31,7 +31,7 @@ def test_all_guilded_rose():
             Conjured("Conjured Mana Cake", 3, 6)], 1)
     items.update_items()
     items_updated = Gilded_rose([
-            Conjured("+5 Dexterity Vest", 9, 18),
+            Conjured("+5 Dexterity Vest", 9, 19),
             Aged_brie("Aged Brie", 1, 1),
             Normal_item("Elixir of the Mongoose", 4, 6),
             Sulfuras_hand("Sulfuras, Hand of Ragnaros", 0, 80),
@@ -70,7 +70,7 @@ Conjured Mana Cake, 2, 4
 
 -------- day 2 --------
 name, sellIn, quality
-+5 Dexterity Vest, 8, 16
++5 Dexterity Vest, 8, 18
 Aged Brie, 0, 2
 Elixir of the Mongoose, 3, 5
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -82,7 +82,7 @@ Conjured Mana Cake, 1, 2
 
 -------- day 3 --------
 name, sellIn, quality
-+5 Dexterity Vest, 7, 14
++5 Dexterity Vest, 7, 17
 Aged Brie, -1, 4
 Elixir of the Mongoose, 2, 4
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -94,7 +94,7 @@ Conjured Mana Cake, 0, 0
 
 -------- day 4 --------
 name, sellIn, quality
-+5 Dexterity Vest, 6, 12
++5 Dexterity Vest, 6, 16
 Aged Brie, -2, 6
 Elixir of the Mongoose, 1, 3
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -106,7 +106,7 @@ Conjured Mana Cake, -1, 0
 
 -------- day 5 --------
 name, sellIn, quality
-+5 Dexterity Vest, 5, 10
++5 Dexterity Vest, 5, 15
 Aged Brie, -3, 8
 Elixir of the Mongoose, 0, 2
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -118,7 +118,7 @@ Conjured Mana Cake, -2, 0
 
 -------- day 6 --------
 name, sellIn, quality
-+5 Dexterity Vest, 4, 8
++5 Dexterity Vest, 4, 14
 Aged Brie, -4, 10
 Elixir of the Mongoose, -1, 0
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -130,7 +130,7 @@ Conjured Mana Cake, -3, 0
 
 -------- day 7 --------
 name, sellIn, quality
-+5 Dexterity Vest, 3, 6
++5 Dexterity Vest, 3, 13
 Aged Brie, -5, 12
 Elixir of the Mongoose, -2, 0
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -142,7 +142,7 @@ Conjured Mana Cake, -4, 0
 
 -------- day 8 --------
 name, sellIn, quality
-+5 Dexterity Vest, 2, 4
++5 Dexterity Vest, 2, 12
 Aged Brie, -6, 14
 Elixir of the Mongoose, -3, 0
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -154,7 +154,7 @@ Conjured Mana Cake, -5, 0
 
 -------- day 9 --------
 name, sellIn, quality
-+5 Dexterity Vest, 1, 2
++5 Dexterity Vest, 1, 11
 Aged Brie, -7, 16
 Elixir of the Mongoose, -4, 0
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -166,7 +166,7 @@ Conjured Mana Cake, -6, 0
 
 -------- day 10 --------
 name, sellIn, quality
-+5 Dexterity Vest, 0, 0
++5 Dexterity Vest, 0, 10
 Aged Brie, -8, 18
 Elixir of the Mongoose, -5, 0
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -178,7 +178,7 @@ Conjured Mana Cake, -7, 0
 
 -------- day 11 --------
 name, sellIn, quality
-+5 Dexterity Vest, -1, 0
++5 Dexterity Vest, -1, 8
 Aged Brie, -9, 20
 Elixir of the Mongoose, -6, 0
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -190,7 +190,7 @@ Conjured Mana Cake, -8, 0
 
 -------- day 12 --------
 name, sellIn, quality
-+5 Dexterity Vest, -2, 0
++5 Dexterity Vest, -2, 6
 Aged Brie, -10, 22
 Elixir of the Mongoose, -7, 0
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -202,7 +202,7 @@ Conjured Mana Cake, -9, 0
 
 -------- day 13 --------
 name, sellIn, quality
-+5 Dexterity Vest, -3, 0
++5 Dexterity Vest, -3, 4
 Aged Brie, -11, 24
 Elixir of the Mongoose, -8, 0
 Sulfuras, Hand of Ragnaros, 0, 80
@@ -214,7 +214,7 @@ Conjured Mana Cake, -10, 0
 
 -------- day 14 --------
 name, sellIn, quality
-+5 Dexterity Vest, -4, 0
++5 Dexterity Vest, -4, 2
 Aged Brie, -12, 26
 Elixir of the Mongoose, -9, 0
 Sulfuras, Hand of Ragnaros, 0, 80
